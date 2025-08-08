2ac5deb2bc60baa13d738d79b8e8e8c02025fb22
zeeshan13 <96335670+Zeeshan120@users.noreply.github.com>
2025-08-08 10:54:25 +0530

    Add files via upload

---
+++ stockprices result.py
@@ -0,0 +1,17 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/zeeshAN/OneDrive/Desktop/hardcoded stock.py ========
+Welcome to the Simple Stock Tracker!
+Available stocks: AAPL, TSLA, GOOGL, MSFT, AMZN
+Enter stock symbol (or type 'done' to finish): GOOGL
+Enter quantity of GOOGL: 20
+Enter stock symbol (or type 'done' to finish): MSFT
+Enter quantity of MSFT: 50
+Enter stock symbol (or type 'done' to finish): DONE
+
+Investment Summary:
+GOOGL - 20 shares - $54000
+MSFT - 50 shares - $16500
+Total Investment: $70500
+Do you want to save the result to a file? (yes/no): Y
